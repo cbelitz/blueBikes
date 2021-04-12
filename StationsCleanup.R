9e6302056838b52fab0a000e47048bcdf18dc0d1
Phil Graff <pgraff@illinois.edu>
2021-04-12 14:49:16 -0500

    adding station cleanup, EDA - exploratory data analysis folder

---
+++ StationsCleanup.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+# Ride data CSV location
+stationfile <- "data/blueBikes/201909-bluebikes-tripdata.csv"
+
+# Read in original data to dataframe from CSV
+ridestations <- read.csv(stationfile)
+attach(ridestations)
+
+# Put all station info (start and end) into same columns
+stackstations <- data.frame(c(start.station.id, end.station.id), c(start.station.name,end.station.name), c(start.station.latitude,end.station.latitude),c(start.station.longitude,end.station.longitude))
+
+# Find list of unique stations
+uniquestations <- unique(stackstations)
+names(uniquestations) <- c("id", "Name", "Latitude", "Longitude")
+
+# Current Stations
+currentstationfile <- "data/blueBikes/current_bluebikes_stations.csv"
+
+# Read current stations to dataframe from CSV
+currentstations <- read.csv(currentstationfile)
+
+# Find stations in the Ride data that are missing in the Current Stations CSV
+missing <- anti_join(uniquestations, currentstations, by = "Name")
+
+
+# Write the Unique Stations out to CSV
