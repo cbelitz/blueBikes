489f34067deb72257385267bc4e023103e6baf51
Phil Graff <pgraff@illinois.edu>
2021-04-16 17:42:05 -0500

    Measurements for bike lanes and T-stops

--- StationsCleanup.R
+++ StationsCleanup.R
@@ -1,4 +1,8 @@
+#install.packages("tidyverse") # For general data purposes
+#install.packages("sf") # For geospatial
+
 library(tidyverse)
+library(sf)
 
 ###################
 ### IMPORT DATA ###
@@ -41,8 +45,7 @@ missing_beforeclean <- anti_join(uniquestations, currentstations, by = "Name")
 
 # Errors in Station Name
 # sub (RIDE name, STATION CSV)
-uniquestations %>% filter(., Name == "Sennott Park  Broadway at Norfolk Street")
-uniquestations$Name <- sub("","",uniquestations$Name)
+#uniquestations$Name <- sub("","",uniquestations$Name)
 
 uniquestations$Name <- sub("Allston Green District - Griggs St at Commonwealth Ave","Commonwealth Ave at Griggs St",uniquestations$Name)
 uniquestations$Name <- sub("Big Papi Station","Lansdowne T Stop",uniquestations$Name)
@@ -61,16 +64,59 @@ uniquestations$Name <- sub("Upham's Corner T Stop - Magnolia St at Dudley St","U
 missing_afterclean <- anti_join(uniquestations, currentstations, by = "Name")
 
 
-# Load the RIDE station ID into the CURRENT STATIONS so that we can join on RIDE ID
+# OPTIONAL Write Missing stations to CSV for further investigation
+#missingcsvfile <- "data/eda/sept2019stns_miss_currentstns.csv"
+#write_csv(missing_afterclean, missingcsvfile)
 
 
+### JOIN DATA FOR STATION DETAILS ###
+# Join the provided Station data for additional attributes (District, Public, Total.docks, minus the lat/longs)
+stations <- left_join(uniquestations, currentstations[,c(1:2,5:7)], by = "Name")
 
+
+######################
+### CALC DISTANCES ###
+######################
+
+# Using Massachusetts State Plane meters, EPSG: 26986
+# https://www.mass.gov/info-details/overview-of-massgis-data#spatial-reference-of-massgis-data-
+
+# Create Point features from Lat/Long
+temp <- st_as_sf(stations, coords=c("Longitude", "Latitude"), crs = 4326, agr = "constant")
+#st_is_longlat(temp)
+
+# Reproject features to Mass State Plane for measuring distance
+stations <- st_transform(temp, crs = 26986)
+
+### LOAD BIKELANES AND TSTOPS ###
+bikelanes <- read_sf("data/gis/bikelanes_ma.shp")
+st_crs(bikelanes) # check projection
+
+tstops <- read_sf("data/gis/tstops.shp")
+st_crs(tstops)
+
+### Find the nearest feature for each bike station, then calculate the distance to that feature
+nearest_bikelane_id <- st_nearest_feature(stations, bikelanes)
+stations$bikelanedist <- st_distance(stations, bikelanes[nearest_bikelane_id,], by_element = TRUE)
+
+nearest_tstop_id <- st_nearest_feature(stations, tstops)
+stations$tstopdist <- st_distance(stations, tstops[nearest_tstop_id,], by_element = TRUE)
+
+### Find the nearest station. Comparing station to station, remove current one: https://stackoverflow.com/questions/49200458/find-nearest-features-using-sf-in-r
+closest <- numeric()
+for(i in seq_len(nrow(stations))){
+  #closest[[i]] <- stations[which.min(st_distance(stations[-i,], stations[i,])),]
+  closest[i] <- st_distance(stations[i,], stations[-i,])
+}
+
+stations$stndist <- closest
+
+# Write a shapefile for data verification
+write_sf(stations, "data/gis/TESTstations.shp")
 
 # Write the Unique Stations out to CSV
 uniquecsvfile <- "data/gis/sept2019stns.csv"
 write_csv(uniquestations, uniquecsvfile)
 
 
-# Write Missing stations to CSV
-missingcsvfile <- "data/eda/sept2019stns_miss_currentstns.csv"
-write_csv(missing, missingcsvfile)
+
