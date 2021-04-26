ca5bd37fc858f89a1ac62c374bae5f708d9ce6cb
cbelitz <clarabelitz1@gmail.com>
2021-04-26 11:33:05 -0400

    remove some mentions of Boston

--- tree_models.R
+++ tree_models.R
@@ -17,8 +17,15 @@ trips_per_day_09_19$Public = as.factor(trips_per_day_09_19$Public)
 
 # try one try to start
 set.seed(1)
-rf.trips = randomForest(flow~weekday+Name+District+Public+Total.docks+bikelanedist+tstopdist, data=trips_per_day_09_19, subset=train, mtry=6, ntree=25, importance=TRUE)
-yhat.rf = predict(rf.boston, newdata=Boston[-train,])
-mean((yhat.rf-boston.test)^2)
-importance(rf.boston)
-varImpPlot(rf.boston)   
+rf.trips = randomForest(flow~weekday+Name+District+Public+Total.docks+bikelanedist+tstopdist,
+                        data=trips_per_day_09_19,
+                        subset=train,
+                        mtry=6,
+                        ntree=25,
+                        na.action=na.exclude,
+                        importance=TRUE)
+yhat.rf = predict(rf.trips, newdata=trips_per_day_09_19[-train,])
+#need to figure out how to skip NA
+mean((yhat.rf-trips.test)^2)
+importance(rf.trips)
+varImpPlot(rf.trips)   
