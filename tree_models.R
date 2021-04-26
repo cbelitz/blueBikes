0e688bbc891b216abde2d0f50f87c239570184d8
cbelitz <clarabelitz1@gmail.com>
2021-04-26 11:24:44 -0400

    initial tree models

---
+++ tree_models.R
@@ -0,0 +1,24 @@
+# install.packages("randomForest")
+# install.packages("gbm")
+
+library(randomForest)
+library(gbm)
+
+# get training set
+set.seed(1)
+train = sample(1:nrow(trips_per_day_09_19), nrow(trips_per_day_09_19)/4)
+trips.test=trips_per_day_09_19[-train ,"flow"]
+
+# set factors
+trips_per_day_09_19$weekday = as.factor(trips_per_day_09_19$weekday)
+trips_per_day_09_19$District = as.factor(trips_per_day_09_19$District)
+trips_per_day_09_19$Public = as.factor(trips_per_day_09_19$Public)
+
+
+# try one try to start
+set.seed(1)
+rf.trips = randomForest(flow~weekday+Name+District+Public+Total.docks+bikelanedist+tstopdist, data=trips_per_day_09_19, subset=train, mtry=6, ntree=25, importance=TRUE)
+yhat.rf = predict(rf.boston, newdata=Boston[-train,])
+mean((yhat.rf-boston.test)^2)
+importance(rf.boston)
+varImpPlot(rf.boston)   
