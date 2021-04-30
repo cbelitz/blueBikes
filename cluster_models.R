26708ef3b664056c6a286aee45a930c381b25d22
cbelitz <clarabelitz1@gmail.com>
2021-04-30 14:43:02 -0400

    small updates to clusters

--- cluster_models.R
+++ cluster_models.R
@@ -1,5 +1,5 @@
-library(e1071)
-library(rmse)
+library(mltools)
+library(data.table)
 
 # set factors
 trips_per_day_09_19$weekday = as.factor(trips_per_day_09_19$weekday)
@@ -8,6 +8,19 @@ trips_per_day_09_19$Public = as.factor(trips_per_day_09_19$Public)
 
 # remove missing values
 clean_trips = na.omit(trips_per_day_09_19)
+test = one_hot(as.data.table(no_names))
 
-# pcr
-pr.trips = prcomp(clean_trips)
+test = subset(test, select=-c(1,2,3))
+
+# k-means
+set.seed(1)
+kcluster.trips = kmeans(test, 20, nstart=20)
+kcluster.trips
+plot(test, col=(kcluster.trips$cluster+1))
+
+pr = prcomp(test)
+pr
+Cols=function(vec){
+  cols=rainbow(length(unique(vec)))
+  return(cols[as.numeric(as.factor(vec))])
+}
